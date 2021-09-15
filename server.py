ae5228bb862c4a1e4b7863df5ec7e6df658cef88
saikatsahana77 <saikatsahana77@gmail.com>
2021-09-15 19:23:21 +0530

    Second Commit

--- server.py
+++ server.py
@@ -1,9 +1,60 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return render_template("index.html")
+def index():
+    try:
+        return render_template('index.html', data=request.args.get('v'))
+    except:
+        return render_template('index.html', data="d")
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route('/signup_check', methods=['POST'])
+def signup_check():
+    uname = request.form['uname']
+    pswd = request.form['pass']
+    email = request.form['email']
+    phone = request.form['phone']
+    phone = int(phone)
+
+
+    conn = sqlite3.connect("bookify.db")
+    q1 = "select email from users where email = '{em}'".format(
+        un=uname, ps=pswd, em=email)
+    rows = conn.execute(q1)
+    rows = rows.fetchall()
+    if (len(rows) == 1):
+        return redirect(url_for('index', v="b"))
+
+
+    conn = sqlite3.connect("bookify.db")
+    q1 = "insert into users (username, email, phone, password) values('{un}','{em}','{ph}','{ps}')".format(un=uname, em=email, ph=phone,  ps=pswd)
+    conn.execute(q1)
+    conn.commit()
+    conn.close()
+    return redirect(url_for('index', v="a"))
+
+
+@app.route('/login_check', methods=['POST'])
+def login_check():
+    email = request.form['email']
+    pswd = request.form['pass']
+    conn = sqlite3.connect("bookify.db")
+    q1 = "select username, password from users where email = '{em}' and password = '{ps}'".format(
+        em=email, ps=pswd)
+    rows = conn.execute(q1)
+    rows = rows.fetchall()
+    conn.close()
+    if (len(rows) == 1):
+        return redirect(url_for('dashboard'))
+    else:
+        return redirect(url_for('index', v="c"))
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/js/home.js
@@ -0,0 +1,41 @@
+function detect() {
+    document.getElementById("password1").classList.add("pass_mismatch");
+    document.getElementById("passHelp1").style.display = "block";
+}
+
+function passWeak() {
+    document.getElementById("passHelp").style.display = "block";
+}
+
+var signup = document.getElementById("signup");
+
+signup.addEventListener("submit", function(e) {
+    var psd = document.getElementById("password").value;
+    var conf_psd = document.getElementById("password1").value;
+    var pass_regex=  newRegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))');
+    var errors= 0;
+    if (psd !== conf_psd){
+        errors+=1;
+        detect()
+    }
+    if(psd.test(pass_regex) == false){
+        errors+=1;
+        passWeak();
+    } 
+    if (errors > 0)
+    {
+        e.preventDefault();
+    }
+})
+
+function signup_success(){
+    swal("Signup Success", "Please login to proceed further!", "success");
+}
+
+function signup_failure(){
+    swal("Signup Failed", "User is already registered!", "error");
+}
+
+function login_failure(){
+    swal("Login Failed", "Invalid email or password!", "error");
+}
