695e0d64214a596cd2d20df3b90f88429826b484
saikatsahana77 <saikatsahana77@gmail.com>
2021-09-20 17:18:10 +0530

    Regular Updates

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -10,3 +10,54 @@ document.getElementById("sell_btn").setAttribute("href",k);
 
 k = "../products/"+sessionStorage.getItem("email");
 document.getElementById("product_btn").setAttribute("href",k);
+
+
+// var items = document.querySelectorAll(".items");
+// var search = document.getElementById("search_btn");
+// var query = document.getElementById("search");
+// var tags = document.querySelectorAll(".tags");
+
+
+// search.addEventListener("click", function() {
+//     console.log("running");
+//     console.log(tags);
+//     tags.forEach((idx) => {
+//         var string = idx.innerHTML;
+//         var substring = query.value;
+//         if (string.indexOf(substring) !== -1){
+//             console.log(idx.parentElement);
+//             var el = idx.parentElement;
+//             // while(el.className != 'items'){
+//             //     el = el.parentElement;
+//             // }
+//             el.style.display = "block";
+//         }
+//         else{
+//             console.log(this);
+//             var el = idx.parentElement;
+//             // while(el.className != 'items'){
+//             //     el = el.parentElement;
+//             // }
+//             el.style.display = "none";
+//         }
+//     })
+// });
+
+
+function filter() {
+    var filter = document.getElementById("search_box").value.toUpperCase();
+    var items = document.querySelectorAll(".items");
+    // var tags = items.getElementsByClassName("tags");
+
+    for (var i= 0; i < items.length; i++){
+        var tag = items[i].querySelectorAll(".tags")[0];
+        if (tag){
+            let value = tag.textContent || tag.innerHTML;
+            if (value.toUpperCase().indexOf(filter) > -1){
+                items[i].style.display = "";
+            }else{
+                items[i].style.display = "none";
+            }
+        }
+    }
+}
