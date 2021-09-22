a11743d5c10669e4bb13b7c3997c4606225c736c
Gourab Chatterjee <gourabchatterjee7777@gmail.com>
2021-09-22 16:33:46 +0530

    documented python script for backend

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+# Importing modules
 import smtplib
 import random
 import math
@@ -13,18 +14,20 @@ import numpy as np
 from PIL import Image
 from instamojo_wrapper import Instamojo
 
-
+# Setting the API keys for payment gateway
 API_KEY = "test_e486fd7dc9a74a4bd030e7e5e0a"
 
 AUTH_TOKEN = "test_c176b5eb749d5d0fe752042e405"
 
+# Initialising the payement gateway API
 api = Instamojo(api_key=API_KEY,auth_token=AUTH_TOKEN,endpoint='https://test.instamojo.com/api/1.1/')
 
-
+# Creating and configuring a flask app
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = r"sqlite:///C:\Users\Asus\Desktop\HackNPitch\bookify.db"
-db = SQLAlchemy(app)
 
+# Creating a sqlalchemy object and attaching it to a model
+db = SQLAlchemy(app)
 class Transactions(db.Model):
     b_name = db.Column(db.String)
     seller = db.Column(db.String)
@@ -41,18 +44,19 @@ class Transactions(db.Model):
     c_image  = db.Column(db.String)
     id  = db.Column(db.Integer, primary_key=True)
 
-
+# Function to convert a file into binary
 def convert_into_binary(file_path):
     with open(file_path, 'rb') as file:
         binary = file.read()
     return binary
 
+# Function to read a file storage object
 def readImage(img_name):
     with open(img_name, 'rb') as file:
         img = file.stream.read()
         return img
 
-
+# Function to send otp
 def otp_send(email, porpouse):
     sender_email = "bookify.suuport@gmail.com"
     receiver_email = email
@@ -80,7 +84,7 @@ def otp_send(email, porpouse):
 
 username = ""
 
-
+# Setting the home route
 @app.route('/')
 def index():
     global username
@@ -90,17 +94,23 @@ def index():
     except:
         return render_template('index.html', data="e")
 
-
+# Setting the dashboard route
 @app.route('/dashboard/<email>')
 def dashboard(email):
     email=str(email)
+
+    # Connecting to database
     conn = sqlite3.connect("bookify.db")
     q1 = "select b_name, bought, seller, pages, age, subject, stream, weight, price, desc, tags, c_image, buyer, id from transactions order by id desc".format(em=email)
     rows = conn.execute(q1)
     rows = rows.fetchall()
     print(rows)
+
+    # Changing the tuples to lists
     for i in range(len(rows)):
         rows[i] = list(rows[i])
+
+    # Removing rows of items that have either been bought or added to cart
     for idx,i in enumerate(rows.copy()):
         print(i[1])
         print(i[12])
@@ -112,22 +122,9 @@ def dashboard(email):
                 rows.remove(i)
             else:
                 pass
-        # if (i[4]== 1):
-        #     rows[idx][4] = "Less than 1 year"
-        # elif (i[4]== 2):
-        #     rows[idx][4] = "Less than 2 years"
-        # elif (i[4]== 3):
-        #     rows[idx][4] = "Less than 5 years"
-        # elif (i[4]== 4):
-        #     rows[idx][4] = "More than 5 years"
     return render_template("dashboard.html", rows= rows)
 
-
-# Less than 1 year
-# Less than 2 years
-# Less than 5 years
-# More than 5 years
-
+# Insrting a new user to the database
 @app.route('/signup_check', methods=['POST'])
 def signup_check():
     uname = request.form['uname']
@@ -136,16 +133,18 @@ def signup_check():
     phone = request.form['phone']
     phone = int(phone)
 
-
+    # Connecting to the database
     conn = sqlite3.connect("bookify.db")
     q1 = "select email from users where email = '{em}'".format(
         un=uname, ps=pswd, em=email)
     rows = conn.execute(q1)
     rows = rows.fetchall()
+    
+    # Checking for duplicate emails as we cannot allow it because email is set as primary key in the users table
     if (len(rows) == 1):
         return redirect(url_for('index', v="b"))
 
-
+    # Insrting a new row i.e. a new user to a database
     conn = sqlite3.connect("bookify.db")
     q1 = "insert into users (username, email, phone, password) values('{un}','{em}','{ph}','{ps}')".format(un=uname, em=email, ph=phone,  ps=pswd)
     conn.execute(q1)
@@ -153,18 +152,22 @@ def signup_check():
     conn.close()
     return redirect(url_for('index', v="a"))
 
-
+# Checking a new user's login credentials
 @app.route('/login_check', methods=['POST'])
 def login_check():
     global username
     email = request.form['email']
     pswd = request.form['pass']
+
+    # Connecting to database
     conn = sqlite3.connect("bookify.db")
     q1 = "select username, password, email, p_updated from users where email = '{em}' and password = '{ps}'".format(
         em=email, ps=pswd)
     rows = conn.execute(q1)
     rows = rows.fetchall()
     conn.close()
+
+    # Redirecting to profile page or dashboard page based on if the user has completed filling his/her profile 
     if (len(rows) == 1):
         print(rows[0][3])
         if (rows[0][3]==0):
@@ -174,12 +177,14 @@ def login_check():
     else:
         return redirect(url_for('index', v="c"))
 
+# Sending otp for password reset
 @app.route('/send_otp', methods=['POST'])
 def send_otp():
     email = request.form['email']
     otp = otp_send(email,"password reset")
     return otp
 
+# Resetting password of an existing user
 @app.route('/reset_password', methods=['POST'])
 def reset_password():
     email = request.form['email_final']
@@ -191,34 +196,45 @@ def reset_password():
     conn.close()
     return redirect(url_for('index', v="d"))
 
+# Route for the profile page
 @app.route('/profile/<email>')
 def profile(email):
     email=str(email)
+
+    # Connecting to database
     conn = sqlite3.connect("bookify.db")
     q1 = "select * from users where email = '{em}'".format(em=email)
     rows = conn.execute(q1)
     rows = rows.fetchall()
     rows[0] = list(rows[0])
+
+    # Setting values to be blank if any null value exists
     for i in range(0,len(rows[0])):
         if (rows[0][i] == None):
             rows[0][i] = ""
     print(rows)
     return render_template("profile.html",rows=rows)
 
+# Route for the upload page
 @app.route('/upload/<email>')
 def upload(email):
     email=str(email)
+
+    # Connecting to database
     conn = sqlite3.connect("bookify.db")
     q1 = "select username, email from users where email = '{em}'".format(em=email)
     rows = conn.execute(q1)
     rows = rows.fetchall()
     rows[0] = list(rows[0])
+
+    # Setting values to be blank if any null value exists
     for i in range(0,len(rows[0])):
         if (rows[0][i] == None):
             rows[0][i] = ""
     print(rows)
     return render_template("upload.html",rows=rows)
 
+# Post route for adding a new book
 @app.route('/add_book', methods=['POST'])
 def add_book():
     bname = request.form['name_book']
@@ -234,6 +250,8 @@ def add_book():
     desc = request.form['desc_book']
     tags = request.form['tags_book']
     c_image = request.files['pic_book']
+
+    # Creating a random filename with the extension of the file entered by the user and saving the file at that location
     _, file_extension = os.path.splitext(c_image.filename)
     filename_1 = str(random.randint(1, 1000000000))
     filename = filename_1 + file_extension
@@ -243,12 +261,14 @@ def add_book():
     c_image = np.array(c_image)
     c_image  = cv2.cvtColor(c_image, cv2.COLOR_BGR2RGB)
     cv2.imwrite(filepath, c_image)
+
+    # Instantiating an object of the transaction class and pushing a new item to the transaction table
     upload = Transactions(b_name= bname, seller = seller, buyer = buyer, bought=bought, pages = pages, age = age, subject = subject, stream = stream, weight = weight, price = price, desc = desc, tags= tags, c_image = filepath)
     db.session.add(upload)
     db.session.commit()
     return redirect("/products/{}".format(seller))
     
-
+# Post route for updating user details
 @app.route('/update_details', methods=['POST'])
 def update_details():
     email = request.form['email']
@@ -256,6 +276,8 @@ def update_details():
     address = request.form['address']
     social = request.form['social']
     upi  = request.form['upi']
+
+    # Connecting to the database and updating the details
     conn = sqlite3.connect("bookify.db")
     q1 = "update users set phone ='{ph}', address='{ad}', social ='{so}', upi='{upi}', p_updated='{pup}' where email='{em}'".format(em=email, ph=phone, ad=address, so=social, upi=upi, pup=1)
     conn.execute(q1)
@@ -263,16 +285,22 @@ def update_details():
     conn.close()
     return redirect("/profile/{}".format(email))
 
-
+# Route for the products page
 @app.route('/products/<email>')
 def method_name(email):
     email=str(email)
+
+    # Connecting to database
     conn = sqlite3.connect("bookify.db")
     q1 = "select b_name, buyer, bought, pages, age, subject, stream, weight, price, desc, tags, c_image from transactions where seller = '{em}' order by id desc".format(em=email)
     rows = conn.execute(q1)
     rows = rows.fetchall()
+
+    # Changing the tuples to lists
     for i in range(len(rows)):
         rows[i] = list(rows[i])
+
+    # Setting appropriate values for buyer and bought columns
     for i in range(len(rows)):
         if (rows[i][1] == None):
             rows[i][1] = "N/A"
@@ -284,11 +312,12 @@ def method_name(email):
             rows[i][2] = "sold"
     return render_template("my_products.html", rows=rows)
 
+# Route for the about page
 @app.route('/about')
 def about():
     return render_template("about.html")
 
-
+# Post route for adding an object to cart - setting buyer to the logged in account 
 @app.route('/add_cart/<id>', methods=['POST'])
 def add_cart(id):
     email = request.form['email_send_cont']
@@ -299,10 +328,12 @@ def add_cart(id):
     conn.close()
     return redirect("/cart/{}".format(email))
 
-
+# Route for the cart page
 @app.route('/cart/<email>')
 def cart(email):
     email=str(email)
+
+    # Connecting to the database
     conn = sqlite3.connect("bookify.db")
     q1 = "select b_name, bought, seller, pages, age, subject, stream, weight, price, desc, tags, c_image, buyer, id from transactions where buyer = '{em}' order by id desc".format(em=email)
     rows = conn.execute(q1)
@@ -310,9 +341,13 @@ def cart(email):
     q2 = "select phone from users where email = '{em}'".format(em=email)
     rows_1 = conn.execute(q2)
     rows_1 = rows_1.fetchall()
+
+    # Setting tuples to lists
     for i in range(len(rows)):
         rows[i] = list(rows[i])
     print(rows)
+
+    # Removing rows with bought equal to 1
     for i in rows.copy():
         if (i[1] == 1):
             rows.remove(i)
@@ -326,7 +361,7 @@ def cart(email):
     print(ids)
     return render_template("cart.html", rows= rows, rows_1 = rows_1, sum=sum, ids=ids)
 
-
+# Post route for removing a book from the cart - setting buyer to null
 @app.route('/remove_book/<id>' , methods=['POST'])
 def remove_book(id):
     email = request.form['email_send_cont']
@@ -337,11 +372,13 @@ def remove_book(id):
     conn.close()
     return redirect("/dashboard/{}".format(email))
 
+# Post route for buying a book - payment gateway integration 
 @app.route('/buy_book/<id>', methods=['POST'])
 def buy_book(id):
     email = request.form['email_send_cont']
     price = request.form['price_send_cont']
 
+    # Creating an order to the payment gateway
     response = api.payment_request_create(
         amount=price,
         purpose="Book Buy",
@@ -351,6 +388,7 @@ def buy_book(id):
         redirect_url="http://localhost:5000/purchases/{}".format(email)
         )
 
+    # Setting bought = 1 for the item
     conn = sqlite3.connect("bookify.db")
     q1 = "update transactions set bought='1' where id='{id}'".format(id=id)
     conn.execute(q1)
@@ -359,12 +397,14 @@ def buy_book(id):
     
     return redirect(response['payment_request']['longurl'])
 
+# Post route for buying many books together - payment gateway integration
 @app.route('/buy_lot/<ids>', methods=['POST'])
 def buy_lot(ids):
     ids = ids.split(" ")
     email = request.form['email_send_cont']
     price = request.form['price_inp']
 
+    # Creating an order to the payment gateway
     response = api.payment_request_create(
         amount=price,
         purpose="Book Buy",
@@ -374,6 +414,7 @@ def buy_lot(ids):
         redirect_url="http://localhost:5000/purchases/{}".format(email)
         )
 
+    # Setting bought = 1 for all the ids we received on the route request
     conn = sqlite3.connect("bookify.db")
     for id in ids:
         q1 = "update transactions set bought='1' where id='{id}'".format(id=id)
@@ -383,20 +424,28 @@ def buy_lot(ids):
     
     return redirect(response['payment_request']['longurl'])
 
+# Setting a route for the login error page
 @app.route('/login_error')
 def login_error():
     return render_template("login_error.html")
 
+# Setting a route for the purchases page
 @app.route('/purchases/<email>')
 def purchases(email):
     email=str(email)
+
+    # Connecting to database
     conn = sqlite3.connect("bookify.db")
     q1 = "select b_name, bought, seller, pages, age, subject, stream, weight, price, desc, tags, c_image, buyer, id from transactions where buyer='{em}' order by id desc".format(em=email)
     rows = conn.execute(q1)
     rows = rows.fetchall()
     print(rows)
+
+    # Changing tuples to lists
     for i in range(len(rows)):
         rows[i] = list(rows[i])
+
+    # Removing items that are unsold i.e. bought = 0 
     for idx,i in enumerate(rows.copy()):
         print(i[1])
         print(i[12])
@@ -407,13 +456,15 @@ def purchases(email):
             pass
     return render_template("my_purchases.html", rows=rows)
 
-
+# Creating post route for sending otp to validate email
 @app.route('/send_otp_check', methods=['POST'])
 def send_otp_check():
     email = request.form['email']
     otp = otp_send(email, "email verification")
     return otp
 
-
+# Main method (starting point of the program)
 if __name__ == '__main__':
+
+    # Running the flask app
     app.run(debug=True)
